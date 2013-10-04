73af38acf855286e3b9b1b8d716b3f72e8bd1ee9
Eduard Bulai <eduard.bulai@zamolsys.ro>
2013-10-04 14:55:11 +0300

    Usability improvements

--- bitcask2riak.rb
+++ bitcask2riak.rb
@@ -1,23 +1,31 @@
 require 'sinatra'
-class Bitcask2Riak < Sinatra::Application
-  require 'sinatra'
-  require 'sidekiq'
-  require 'sidekiq-middleware'
-  require 'redis'
-  require 'awesome_print'
-  require 'bitcask'
-  require 'bert'
-  require 'riak'
+require 'sidekiq'
+require 'sidekiq-middleware'
+require 'redis'
+require 'awesome_print'
+require 'bitcask'
+require 'bert'
+require 'riak'
+require 'yaml'
 
+class Bitcask2Riak < Sinatra::Application
+
+  $config = YAML.load_file('./config/config.yaml')
   # to change the redis server just run this in the terminal
   # export REDIS_URL='redis://host:port'
-  $redis = Redis.new
 
   Sidekiq.configure_server do |config|
     config.server_middleware do |chain|
       chain.add Sidekiq::Middleware::Server::UniqueJobs
     end
+    config.redis = { :url => $config['redis']['url'], :namespace => $config['redis']['namespace'] }
   end
+
+  Sidekiq.configure_client do |config|
+    config.redis = { :url => $config['redis']['url'], :namespace => $config['redis']['namespace'] }
+  end
+
+  #$redis = Redis.new({:url => $config['redis']['url'], :namespace => $config['redis']['namespace']})
 
   class BitcaskWorker
     include Sidekiq::Worker
@@ -30,18 +38,23 @@ class Bitcask2Riak < Sinatra::Application
       sep2 = "}\x00\x00\x00"
       b.data_files.each do |data_file|
         if data_file.count > 0 
+          stats = Sidekiq::Stats.new
+          ap stats.enqueued
           data_file.each do |entry|
-            next if entry.value == Bitcask::TOMBSTONE
-            # Get Data
-            key_decode = BERT.decode(entry.key)       
-            bucket = key_decode[0]
-            key = key_decode[1]
-            begin
+            unless stats.enqueued.to_i < 10000
+              puts "Too much jobs. Sleeping for 60 seconds...\r\n"
+              sleep 60
+              redo  
+            else
+              next if entry.value == Bitcask::TOMBSTONE
+              # Get Data
+              key_decode = BERT.decode(entry.key)       
+              bucket = key_decode[0]
+              key = key_decode[1]
               value = BERT.decode(entry.value).last
-            rescue
-              value = "{\"" + entry.value[/#{sep1}(.*?)#{sep2}/m, 1].to_s + "}"
+              worker = RiakWorker.perform_async(bucket, key, value)
+              puts "Enqueued #{worker.inspect}"
             end
-            RiakWorker.perform_async(bucket, key, value)
           end
         end
       end
@@ -52,6 +65,7 @@ class Bitcask2Riak < Sinatra::Application
     include Sidekiq::Worker
     sidekiq_options :retry => 20, :queue => :heavy
     sidekiq_options({unique: :all, expiration: 24 * 60 * 60})
+
     def perform(bucket, key, val)
       # Retrieve a bucket
       # Create a client interface
@@ -72,14 +86,17 @@ class Bitcask2Riak < Sinatra::Application
     '<h1>Bitcask to Riak Mover</h1>
 
     <form method="post" action="/do">
-      <input type="text" name="msg">
-      <input type="submit" value="Add Folder">
+      <input type="text" name="dir">
+      <input type="submit" value="Add Bitcask Folder">
     </form>
     <a href="/sidekiq" target="_blank">View stats</a>'
   end
 
   post '/do' do
-    BitcaskWorker.perform_async params[:msg]
+    partitions = Dir.glob("#{params[:dir]}/**")
+    partitions.each do |partition|
+      BitcaskWorker.perform_async(partition)
+    end
     redirect to('/')
   end
   # End Sinatra app
